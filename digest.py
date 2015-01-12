a82f14842ddd923165eddddd686df03657a0aa16
Antonio Rebordao <antonio@rebordao.net>
2015-01-12 16:47:37 +0000

    Improved digest.py

--- digest.py
+++ digest.py
@@ -13,35 +13,34 @@ import smtplib
 import imaplib
 
 from email.mime.text import MIMEText
-from email.header import Header
 
-def open_smtp_conn(email_cfg):
+def open_smtp_conn(email_cfg, verbose = True):
     '''
     Opens SMTP connection.
     '''
 
     # Creates SMTP connection
-    print "Connecting to SMTP server..."
+    if verbose: print "Connecting to SMTP server..."
     conn = smtplib.SMTP(email_cfg['smtp_server'])
     conn.starttls()
 
     # Log in
-    print "Logging in as {:s}".format(email_cfg['username'])
+    if verbose: print "Logging in as '{:s}'".format(email_cfg['username'])
     conn.login(email_cfg['username'], email_cfg['password'])
 
     return(conn)
 
-def open_imap_conn(email_cfg):
+def open_imap_conn(email_cfg, verbose = True):
     '''
     Opens IMAP connection.
     '''
 
     # Creates IMAP connection
-    print "Connecting to IMAP server..."
+    if verbose: print "Connecting to IMAP server..."
     conn = imaplib.IMAP4_SSL('imap.gmail.com')
 
     # Log in
-    print "Logging in as {:s}".format(email_cfg['username'])
+    if verbose: print "Logging in as '{:s}'".format(email_cfg['username'])
     conn.login(email_cfg['username'], email_cfg['password'])
     conn.list()
 
@@ -71,22 +70,24 @@ def parse_raw_email(raw_email):
     elif maintype == 'text':
         return(raw_email.get_payload())
 
-def get_raw_emails(*args):
+def get_raw_emails(verbose = True, *args):
     '''
     Reads yesterday's replyies.
     '''
 
     # Opens IMAP conn
-    imap_conn = open_imap_conn(email_cfg)
+    imap_conn = open_imap_conn(cfg['email_cfg'])
 
     # Builds subject of yesterday
     yesterday = datetime.date.today() - datetime.timedelta(days = 1)
-    subject = "{:s} | {:s}".format(yesterday.strftime('%d-%m-%Y'), list_name[:-4])
+    subject = "{:s} | {:s}".format(
+            yesterday.strftime('%d-%m-%Y'), team['team_name'])
 
     # Gets ids of the emails with that subject
-    fl, ids = imap_conn.search(None, '(HEADER Subject "{:s}")'.format(subject))
+    sta, ids = imap_conn.search(None, '(HEADER Subject "{:s}")'.format(subject))
 
     # Gets raw emails
+    if verbose: print "Reading emails"
     raw_emails = []
     for eid in ids[0].split():
         status, msg_data = imap_conn.fetch(eid, '(RFC822)')
@@ -102,27 +103,43 @@ def build_digest(*args):
     '''
 
     # Gets raw emails
-    raw_emails = get_raw_emails(cfg['email_cfg'], list_name)
+    raw_emails = get_raw_emails(cfg['email_cfg'], team)
 
     # Aggregates them into one digest
     digest = ''
     for raw_email in raw_emails:
-        digest = digest + raw_email['From'] + '\n' + remove_empty_lines(
+        digest += raw_email['From'] + '\n' + remove_empty_lines(
                 parse_raw_email(raw_email)) + '\n'
 
     return(digest)
 
-def clean_digest(digest):
+def clean_digest(digest, verbose = True):
     '''
-    Cleans digest.
+    Preprocess digest.
     '''
 
+    if verbose: print "Cleaning emails"
     clean_digest = ''
     for line in digest.splitlines():
-        if not line.startswith('>') and not "Amigo <reminder@mentis-consulting.be>" in line:
-            clean_digest = clean_digest + line + '\n'
+        if (not line.startswith('>') and 
+                not "Amigo <reminder@mentis-consulting.be>" in line):
+            clean_digest += line + '\n'
 
     return(clean_digest)
+
+def add_missing_members(team, digest):
+    '''
+    Adds indication of the members that didn't reply to the survey.
+    '''
+
+    print "Adding missing members"
+    yesterday = datetime.datetime.today() - datetime.timedelta(days = 1)
+    for member in team['members']:
+        if (yesterday.strftime("%A") in member['availability'] and 
+                not member['email'] in digest):
+            digest += "\n{:s} <{:s}>".format(
+                member['name'], member['email']) + "\nDid not reply to the survey.\n"
+    return(digest)
 
 if __name__ == '__main__':
 
@@ -130,10 +147,14 @@ if __name__ == '__main__':
     cfg = yaml.load(open('config.yml', 'r'))
 
     # Opens connection to smtp server
-    smtp_conn = open_smtp_conn(email_cfg = cfg['email_cfg'])
+    smtp_conn = open_smtp_conn(cfg['email_cfg'])
 
     # Sends survey to all teams
     for file_name in os.listdir(os.path.join(os.getcwd(), 'lists')):
+
+        # Ignores the team's template (dev_team.yml.EXAMPLE)
+        if "EXAMPLE" in file_name:
+            continue
 
         # Reads team's metadata
         team_file = os.path.join(os.getcwd(), 'lists', file_name)
@@ -145,24 +166,32 @@ if __name__ == '__main__':
         # Cleans digest
         digest = clean_digest(digest)
 
+        # Adding members that didn't reply
+        digest = add_missing_members(team, digest)
+
         # Builds subject of yesterday
         yesterday = datetime.date.today() - datetime.timedelta(days = 1)
         subject = "Digest | {:s} | {:s}".format(
                 yesterday.strftime('%d-%m-%Y'), team['team_name'])
 
         # Sends digest to team
-        print "Processing team '{:s}'".format(team['team_name'])
-        for member in team['members']:
+        print "Sending digest to team '{:s}'".format(team['team_name'])
+        members = [val for val in team['members']]
+        members.append(team['team_leader'])
+        for member in members:
 
             # Creates email headers
-            email = MIMEText(digest, 'plain', 'utf-8')
-            email['Subject'] = Header(subject, 'utf-8')
-            email['From'] = Header(cfg['email_cfg']['sender_name'], 'utf-8')
-            email['To'] = Header(member['name'], 'utf-8')
+            dig_email = MIMEText(digest, 'plain', 'utf-8')
+            dig_email['Subject'] = subject
+            dig_email['From'] = "'{:s}' <{:s}>".format(
+                cfg['email_cfg']['sender_name'], cfg[
+                'email_cfg']['sender_email'])
+            dig_email['To'] = "'{:s}' <{:s}>".format(
+                    member['name'], member['email'])
 
             # Sends email
-            smtp_conn.sendmail(from_addr = cfg['email_cfg']['sender_name'],
-                    to_addrs = member['email'], msg = email.as_string())
+            smtp_conn.sendmail(from_addr = cfg['email_cfg']['sender_name'], 
+                    to_addrs = member['email'], msg = dig_email.as_string())
             time.sleep(3)
 
-        smtp_conn.quit()
+    smtp_conn.quit()
